9601f74778da56a67ade1be29d88afbddb0f0cc8
Olivia Yin <r63875@freescale.com>
2016-05-17 11:32:21 +0800

    remove GPL and Freescale-EULA
    Signed-off-by: Olivia Yin <olivia.yin@nxp.com>
    [Stuart: added missing signed-off for author]
    Signed-off-by: Stuart Yoder <stuart.yoder@nxp.com>

--- fsl_dpcon_cmd.h
+++ fsl_dpcon_cmd.h
@@ -11,12 +11,6 @@
  * names of any contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
  *
- *
- * ALTERNATIVELY, this software may be distributed under the terms of the
- * GNU General Public License ("GPL") as published by the Free Software
- * Foundation, either version 2 of that License or (at your option) any
- * later version.
- *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
