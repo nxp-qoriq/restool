f01f2ea4ab2d5b2432c4497cc772d2fd8cca3eb3
Ioana Ciornei <ioana.ciornei@nxp.com>
2017-06-20 18:47:07 +0300

    add NXP Copyright where necessary
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

--- dpdbg_commands.c
+++ dpdbg_commands.c
@@ -1,4 +1,4 @@
-/* Copyright 2014-2017 Freescale Semiconductor Inc.
+/* Copyright 2014-2016 Freescale Semiconductor Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
