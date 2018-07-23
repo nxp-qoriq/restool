nxp-qoriq/restool

commit 63f6662c3c52dcf798a309c0e5369521f1cf24f3

dpseci: increase the maximum number of queues
Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

dpseci_commands.c
@@ -625,7 +625,7 @@ static int create_dpseci_v10(const char *usage_msg)
 			~ONE_BIT_MASK(CREATE_OPT_PRIORITIES);
 		error = get_option_value(CREATE_OPT_NUM_QUEUES, &val,
 					 "Invalid number of queues range",
-					 1, DPSECI_PRIO_NUM);
+					 1, DPSECI_MAX_QUEUE_NUM);
 		if (error)
 			return -EINVAL;
 		dpseci_cfg.num_tx_queues = val;
@@ -729,7 +729,7 @@ static int cmd_dpseci_create_v10(void)
 	static const char usage_msg_1[] =
 		"\n"
 		"Usage: restool dpseci create --num-queues=<count> --priorities=<pri1,pri2,...> [OPTIONS]\n"
-		"   --num-queues=<number of rx/tx queues>, ranges from 1 to 8\n"
+		"   --num-queues=<number of rx/tx queues>, ranges from 1 to 16\n"
 		"   --priorities=<priority1,priority2, ...,priority8>\n"
 		"      DPSECI supports num-queues priorities that can be individually set.\n"
 		"      if --num-queues=3, then --priorities=X,Y,Z\n"

mc_v10/fsl_dpseci.h
@@ -51,6 +51,11 @@ int dpseci_close_v10(struct fsl_mc_io *mc_io,
 		     uint16_t token);
 
 /**
+ * Maximum number of Tx/Rx priorities per DPSECI object
+ */
+#define DPSECI_MAX_QUEUE_NUM				16
+
+/**
  * Enable the Congestion Group support
  */
 #define DPSECI_OPT_HAS_CG				0x000020
@@ -82,7 +87,7 @@ struct dpseci_cfg_v10 {
 	uint32_t options;
 	uint8_t num_tx_queues;
 	uint8_t num_rx_queues;
-	uint8_t priorities[DPSECI_PRIO_NUM];
+	uint8_t priorities[DPSECI_MAX_QUEUE_NUM];
 };
 
 int dpseci_create_v10(struct fsl_mc_io *mc_io,

commit 80402b8f6588a0c06a0942046745aa283bdd307d

dpseci: fix 16 queues support
16 queues support has been added starting with dpseci object version
v5.3. The format of "create" command changed, thus update restool
accordingly.

Note: dpseci version is being incremented from 5.1 to 5.3; since v5.2
adds support for reporting number of Poly1305 and Chacha20 accelerators
in the GET_SEC_ATTR command, it does not bring any changes in restool.

Fixes: 63f6662c3c52 ("dpseci: increase the maximum number of queues")
Signed-off-by: Horia Geantă <horia.geanta@nxp.com>

dpseci_commands.c
@@ -528,7 +528,7 @@ static int create_dpseci_v9(const char *usage_msg)
 			~ONE_BIT_MASK(CREATE_OPT_PRIORITIES);
 		error = get_option_value(CREATE_OPT_NUM_QUEUES, &val,
 					 "Invalid number of queues range",
-					 1, DPSECI_PRIO_NUM);
+					 1, DPSECI_MAX_QUEUE_NUM);
 		if (error)
 			return -EINVAL;
 		dpseci_cfg.num_tx_queues = val;

mc_v10/dpseci.c
@@ -142,8 +142,10 @@ int dpseci_create_v10(struct fsl_mc_io *mc_io,
 					  cmd_flags,
 					  dprc_token);
 	cmd_params = (struct dpseci_cmd_create *)cmd.params;
-	for (i = 0; i < DPSECI_PRIO_NUM; i++)
+	for (i = 0; i < 8; i++)
 		cmd_params->priorities[i] = cfg->priorities[i];
+	for (i = 0; i < 8; i++)
+		cmd_params->priorities2[i] = cfg->priorities[8 + i];
 	cmd_params->num_tx_queues = cfg->num_tx_queues;
 	cmd_params->num_rx_queues = cfg->num_rx_queues;
 	cmd_params->options = cfg->options;

mc_v10/fsl_dpseci_cmd.h
@@ -35,20 +35,22 @@
 
 /* DPSECI Version */
 #define DPSECI_VER_MAJOR		5
-#define DPSECI_VER_MINOR		1
+#define DPSECI_VER_MINOR		3
 
 /* Command versioning */
 #define DPSECI_CMD_BASE_VERSION		1
 #define DPSECI_CMD_BASE_VERSION_V2	2
+#define DPSECI_CMD_BASE_VERSION_V3	3
 #define DPSECI_CMD_ID_OFFSET		4
 
 #define DPSECI_CMD_V1(id)	((id << DPSECI_CMD_ID_OFFSET) | DPSECI_CMD_BASE_VERSION)
 #define DPSECI_CMD_V2(id)	((id << DPSECI_CMD_ID_OFFSET) | DPSECI_CMD_BASE_VERSION_V2)
+#define DPSECI_CMD_V3(id)	((id << DPSECI_CMD_ID_OFFSET) | DPSECI_CMD_BASE_VERSION_V3)
 
 /* Command IDs */
 #define DPSECI_CMDID_CLOSE		DPSECI_CMD_V1(0x800)
 #define DPSECI_CMDID_OPEN		DPSECI_CMD_V1(0x809)
-#define DPSECI_CMDID_CREATE		DPSECI_CMD_V2(0x909)
+#define DPSECI_CMDID_CREATE		DPSECI_CMD_V3(0x909)
 #define DPSECI_CMDID_DESTROY		DPSECI_CMD_V1(0x989)
 #define DPSECI_CMDID_GET_API_VERSION	DPSECI_CMD_V1(0xa09)
 #define DPSECI_CMDID_GET_ATTR		DPSECI_CMD_V1(0x004)
@@ -76,6 +78,8 @@ struct dpseci_cmd_create {
 	uint8_t num_rx_queues;
 	uint8_t pad[6];
 	uint32_t options;
+	uint32_t pad1;
+	uint8_t priorities2[8];
 };
 
 struct dpseci_cmd_destroy {