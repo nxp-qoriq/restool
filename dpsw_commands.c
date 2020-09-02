0dd87442c755b2fd54d6116ac4544cd8ce3866b6
Ionut-robert Aron <ionut-robert.aron@nxp.com>
2020-09-02 12:27:03 +0300

    restool: add DPSW_OPT_BP_PER_IF in dpsw create and ls-addsw
    Update restool and the helper scripts to accept the new DPSW option -
    DPSW_OPT_BP_PER_IF.
    
    Signed-off-by: Ionut-robert Aron <ionut-robert.aron@nxp.com>

--- dpsw_commands.c
+++ dpsw_commands.c
@@ -49,7 +49,8 @@
 	DPSW_OPT_CTRL_IF_DIS |	\
 	DPSW_OPT_FLOODING_METERING_DIS |	\
 	DPSW_OPT_METERING_EN |		\
-	DPSW_OPT_LAG_DIS)
+	DPSW_OPT_LAG_DIS |		\
+	DPSW_OPT_BP_PER_IF)
 
 static enum mc_cmd_status mc_status;
 
@@ -273,6 +274,7 @@ static struct option_entry options_map[] = {
 	OPTION_MAP_ENTRY(DPSW_OPT_FLOODING_METERING_DIS),
 	OPTION_MAP_ENTRY(DPSW_OPT_METERING_EN),
 	OPTION_MAP_ENTRY(DPSW_OPT_LAG_DIS),
+	OPTION_MAP_ENTRY(DPSW_OPT_BP_PER_IF),
 };
 static unsigned int options_num = ARRAY_SIZE(options_map);
 
@@ -324,6 +326,9 @@ static void print_dpsw_options(uint64_t options)
 
 	if (options & DPSW_OPT_LAG_DIS)
 		printf("\tDPSW_OPT_LAG_DIS\n");
+
+	if (options & DPSW_OPT_BP_PER_IF)
+		printf("\tDPSW_OPT_BP_PER_IF\n");
 }
 
 static int print_dpsw_endpoint(uint32_t target_id, uint16_t num_ifs,
@@ -1062,6 +1067,7 @@ static int cmd_dpsw_create_v10(void)
 		"	DPSW_OPT_FLOODING_METERING_DIS\n"
 		"	DPSW_OPT_METERING_EN\n"
 		"	DPSW_OPT_LAG_DIS\n"
+		"	DPSW_OPT_BP_PER_IF\n"
 		"--max-vlans=<number>\n"
 		"	Maximum number of VLAN's. Default is 16.\n"
 		"--max-fdbs=<number>\n"
