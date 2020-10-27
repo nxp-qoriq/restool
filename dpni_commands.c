nxp-qoriq/restool

commit 1e9c4d20b70810eb16a2506cdb441176028afe62

restool: use --help in all circumstances
Instead of switching between --help and help when trying to get
the help text, make --help the single flag to be used.

Signed-off-by: Ionut-robert Aron <ionut-robert.aron@nxp.com>

dpni_commands.c
@@ -108,14 +108,14 @@ static struct option dpni_info_options[] = {
 		.name = "help",
 		.has_arg = 0,
 		.flag = NULL,
-		.val = 0,
+		.val = 'h',
 	},
 
 	[INFO_OPT_VERBOSE] = {
 		.name = "verbose",
 		.has_arg = 0,
 		.flag = NULL,
-		.val = 0,
+		.val = 'v',
 	},
 
 	{ 0 },
@@ -1904,7 +1904,7 @@ static int cmd_dpni_update_v10(void)
 }
 
 struct object_command dpni_commands_v9[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpni_help },
 
@@ -1924,7 +1924,7 @@ struct object_command dpni_commands_v9[] = {
 };
 
 struct object_command dpni_commands_v10[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpni_help_v10 },
 

dpaiop_commands.c
@@ -775,7 +775,7 @@ static int cmd_dpaiop_destroy_v10(void)
 }
 
 struct object_command dpaiop_commands_v9[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpaiop_help },
 
@@ -795,7 +795,7 @@ struct object_command dpaiop_commands_v9[] = {
 };
 
 struct object_command dpaiop_commands_v10[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpaiop_help },
 

dpbp_commands.c
@@ -639,7 +639,7 @@ static int cmd_dpbp_destroy_v10(void)
 }
 
 struct object_command dpbp_commands_v9[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpbp_help },
 
@@ -659,7 +659,7 @@ struct object_command dpbp_commands_v9[] = {
 };
 
 struct object_command dpbp_commands_v10[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpbp_help },
 

dpci_commands.c
@@ -798,7 +798,7 @@ static int cmd_dpci_destroy_v10(void)
 }
 
 struct object_command dpci_commands_v9[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpci_help },
 
@@ -818,7 +818,7 @@ struct object_command dpci_commands_v9[] = {
 };
 
 struct object_command dpci_commands_v10[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpci_help },
 

dpcon_commands.c
@@ -694,7 +694,7 @@ static int cmd_dpcon_destroy_v10(void)
 }
 
 struct object_command dpcon_commands_v9[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpcon_help },
 
@@ -714,7 +714,7 @@ struct object_command dpcon_commands_v9[] = {
 };
 
 struct object_command dpcon_commands_v10[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpcon_help },
 

dpdbg_commands.c
@@ -743,7 +743,7 @@ static int cmd_dpdbg_create(void)
 
 
 struct object_command dpdbg_commands[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpdbg_help },
 

dpdcei_commands.c
@@ -742,7 +742,7 @@ static int cmd_dpdcei_destroy_v10(void)
 }
 
 struct object_command dpdcei_commands_v9[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpdcei_help },
 
@@ -762,7 +762,7 @@ struct object_command dpdcei_commands_v9[] = {
 };
 
 struct object_command dpdcei_commands_v10[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpdcei_help },
 

dpdmai_commands.c
@@ -754,7 +754,7 @@ static int cmd_dpdmai_destroy_v10(void)
 }
 
 struct object_command dpdmai_commands_v9[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpdmai_help },
 
@@ -774,7 +774,7 @@ struct object_command dpdmai_commands_v9[] = {
 };
 
 struct object_command dpdmai_commands_v10[] = {
-	{ .cmd_name = "help",
+	{ .cmd_name = "--help",
 	  .options = NULL,
 	  .cmd_func = cmd_dpdmai_help },
 

commit 8ddbe4c9559ffad5d7e5dd3cd5f00ceeff7f05d4

dpni: fix info --verbose behavior
The value to be returned by getopt when the info options are found was
changed from 0 to a char value. Because of this, the parse_cmd_options()
function was failing and returning and error even before any command was
issued. Revert the change back to the prior behavior.

Fixes: 1e9c4d20b708 ("restool: use --help in all circumstances")
Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

dpni_commands.c
@@ -110,14 +110,14 @@ static struct option dpni_info_options[] = {
 		.name = "help",
 		.has_arg = 0,
 		.flag = NULL,
-		.val = 'h',
+		.val = 0,
 	},
 
 	[INFO_OPT_VERBOSE] = {
 		.name = "verbose",
 		.has_arg = 0,
 		.flag = NULL,
-		.val = 'v',
+		.val = 0,
 	},
 
 	{ 0 },