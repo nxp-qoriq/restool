nxp-qoriq/restool

commit 66d941a0160d33afba4dd7851315dd5e9d6320af

Adapt BITS_PER_LONG and GENMASK() definitions for 32-bit compatibility
Fixes build errors such as below:

mc_v10/dpci.c: In function ‘dpci_get_link_state_v10’:
mc_v10/fsl_mc_cmd.h:56:28: error: right shift count >= width of type [-Werror=shift-count-overflow]
   56 |   (((~0UL) << (l)) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
      |                            ^~
mc_v10/fsl_dpci_cmd.h:61:2: note: in expansion of macro ‘GENMASK’
   61 |  GENMASK(DPCI_##field##_SHIFT + DPCI_##field##_SIZE - 1, \
      |  ^~~~~~~
mc_v10/fsl_dpci_cmd.h:66:12: note: in expansion of macro ‘DPCI_MASK’
   66 |  (((var) & DPCI_MASK(field)) >> DPCI_##field##_SHIFT)
      |            ^~~~~~~~~
mc_v10/dpci.c:298:8: note: in expansion of macro ‘dpci_get_field’
  298 |  *up = dpci_get_field(rsp_params->up, UP);
      |        ^~~~~~~~~~~~~~

Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

mc_v10/fsl_mc_cmd.h
@@ -51,9 +51,13 @@
 #define le32_to_cpu	le32toh
 #define le16_to_cpu	le16toh
 
-#define BITS_PER_LONG			64
+#ifndef BITS_PER_LONG
+#define BITS_PER_LONG __WORDSIZE
+#endif
+
 #define GENMASK(h, l) \
-		(((~0UL) << (l)) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
+	(((~(unsigned long)0) - ((unsigned long)1 << (l)) + 1) & \
+	 (~(unsigned long)0 >> (BITS_PER_LONG - 1 - (h))))
 
 struct mc_cmd_header {
 	union {

commit b44748ed0bb3c574f8b528b438b2f6ac40beefab

Avoid use of non-portable __WORDSIZE when defining BITS_PER_LONG
Here is the location of __WORDSIZE in various buildroot toolchains:
- glibc, uclibc: /usr/include/bits/wordsize.h
- musl: /usr/include/bits/reg.h

Actually musl defines no <bits/wordsize.h> and I don't see any way in
which we could easily support all toolchain locations of this definition.

Furthermore, <bits/reg.h> is not transitively included by musl, so
compilation currently fails as can be seen below:

In file included from mc_v10/dpdbg.c:35:
mc_v10/dpdbg.c: In function ‘dpdbg_set_ctlu_profiling_counters’:
mc_v10/fsl_mc_cmd.h:55:23: error: ‘__WORDSIZE’ undeclared (first use in this function)
   55 | #define BITS_PER_LONG __WORDSIZE
      |                       ^~~~~~~~~~
mc_v10/fsl_mc_cmd.h:60:33: note: in expansion of macro ‘BITS_PER_LONG’
   60 |          (~(unsigned long)0 >> (BITS_PER_LONG - 1 - (h))))
      |                                 ^~~~~~~~~~~~~
mc_v10/fsl_dpdbg_cmd.h:63:9: note: in expansion of macro ‘GENMASK’
   63 |         GENMASK(DPDBG_##field##_SHIFT + DPDBG_##field##_SIZE - 1, \
      |         ^~~~~~~
mc_v10/fsl_dpdbg_cmd.h:67:55: note: in expansion of macro ‘DPDBG_MASK’
   67 |         ((var) |= (((val) << DPDBG_##field##_SHIFT) & DPDBG_MASK(field)))
      |                                                       ^~~~~~~~~~

and many others.

For the architectures we care about, the __WORDSIZE (32 or 64) can
simply and portably be derived from the size in bytes of a pointer,
multiplied by the size in bits of a byte.

Fixes: 66d941a0160d ("Adapt BITS_PER_LONG and GENMASK() definitions for 32-bit compatibility")
Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

mc_v10/fsl_mc_cmd.h
@@ -52,7 +52,7 @@
 #define le16_to_cpu	le16toh
 
 #ifndef BITS_PER_LONG
-#define BITS_PER_LONG __WORDSIZE
+#define BITS_PER_LONG (sizeof(void *) * 8)
 #endif
 
 #define GENMASK(h, l) \